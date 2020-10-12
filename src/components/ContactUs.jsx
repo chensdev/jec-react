63093a17c3696a31a1b6bbe3e0f5074dfda44c37
Chris <christopher_henson@outlook.com>
2020-10-12 10:23:41 +0100

    update

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -4,7 +4,7 @@ export default function ContactUs() {
 	return (
 		<section id="contact-us">
 			<div className="container--sm">
-				<div className="contact-us__content">Balls</div>
+				<div className="contact-us__content"></div>
 			</div>
 		</section>
 	);
