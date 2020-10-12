6af1351e450747e079d586dfdf352fdea7fc238a
Chris <christopher_henson@outlook.com>
2020-10-12 11:06:06 +0100

    added content, constructed services section (ready for images, lower thirds and carousel plugin implementation)

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -4,8 +4,11 @@ export default function AboutUs() {
 	return (
 		<section id="about-us">
 			<div className="container--sm">
-				<div className="about-content">
-					This is the about section.
+				<div className="about-content"> {/*make this a card*/}
+					<h3>About Us</h3>
+					<h1>Professional Electrical 
+					Contractors in the North East </h1>
+					<p>Our team of skilled and experienced electrical engineersare fully equipped to carry out a wide variety of contractor services, across a variety of industries and sectors.</p>
 					<button className="btn btn--yellow-border">Find out more</button>
 				</div>
 			</div>
