be6e3f7adb7f3a5aece2bbea075a28fc405f0bd7
Chris <christopher_henson@outlook.com>
2020-10-13 16:28:47 +0100

    edited slide data, overlay needed on images

---
+++ README.md
@@ -0,0 +1,9 @@
+img credits:
+Jason Leung
+https://unsplash.com/@ninjason
+
+Nick Karvounis
+https://unsplash.com/@nickkarvounis
+
+Siyuan
+https://unsplash.com/@jsycra
--- src/data/slideData.js
+++ src/data/slideData.js
@@ -1,7 +1,7 @@
 const slideData = [
 	{
 		id: 1,
-		imgurl: `url("https://images.unsplash.com/photo-1572491671313-e2bb1bf15e76?ixlib=rb-1.2.1&auto=format&fit=crop&w=4489&q=80")`,
+		imgurl: `url("https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3750&q=80")`,
 		h1: "slide 1",
 		content: "this is slide 1",
 		btn1: "Explore",
@@ -9,7 +9,7 @@ const slideData = [
 	},
 	{
 		id: 2,
-		imgurl: `url("https://images.unsplash.com/photo-1520853225856-e87762ef8c55?ixlib=rb-1.2.1&auto=format&fit=crop&w=4800&q=80")`,
+		imgurl: `url("https://images.unsplash.com/photo-1552566626-52f8b828add9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3750&q=80")`,
 		h1: "slide 2",
 		content: "this is slide 2",
 		btn1: "Explore",
@@ -17,7 +17,7 @@ const slideData = [
 	},
 	{
 		id: 3,
-		imgurl: `url("https://images.unsplash.com/photo-1553677856-035da1f4500b?ixlib=rb-1.2.1&auto=format&fit=crop&w=4202&q=80")`,
+		imgurl: `url("https://images.unsplash.com/photo-1579027989536-b7b1f875659b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3750&q=80")`,
 		h1: "slide 3",
 		content: "this is side 3",
 		btn1: "Contact",
