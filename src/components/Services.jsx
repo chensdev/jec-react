4a7be9e96262c3a796f44317c129cb75b6dc74b3
Chris <christopher_henson@outlook.com>
2020-10-13 16:16:10 +0100

    slider amendments

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,19 +1,23 @@
 import React from "react";
+import Slider from "react-slick";
 // import Slider from 'react-slick'
 // import slideData from "../data/slideData";
 
 export default function Services() {
-	// const settings = {
-	// 	slidesToShow: 3,
-	// 	slidesToScroll: 1,
-	// 	infinite: true,
-	// 	arrows: true,
-	// }
+	const settings = {
+		slidesToShow: 3,
+		slidesToScroll: 1,
+	}
 	return (
 		// const slideComponent = slideData.map(item => <Slide />)
 		<section id="services">
 			<div className="container">
-				<div className="services-content"><h3>Services</h3>
+				<div className="services-content">
+					<div className="pagination"> {/*connect with custom arrow components*/}
+						<button className="prev"><i class="fas fa-angle-left"></i></button>
+						<button className="next"><i class="fas fa-angle-right"></i></button>
+					</div>
+					<h3>Services</h3>
 					<h1>Commercial, industrial and retail</h1>
 					<p>Carried out by our team of highly skilled 
 					and experienced professionals, all of our services are totally bespoke and can be fully tailored to ensure that they meet 
@@ -21,10 +25,15 @@ export default function Services() {
 					</p>
 					<button className="btn btn--yellow-border">Find out more</button>
 				</div>
-				<div className="servSlide">slide 1</div>
-				<div className="servSlide">slide 2</div>
-				<div className="servSlide">slide 3</div>
-				{/* <Slider {...settings}>{slideComponent}</Slider> */}
+				<div className="services-slider">
+					<Slider {...settings}>
+						<div className="servSlide">
+						<h1>Industrial & Commercial Installations</h1></div>
+						<div className="servSlide"><h1>Specialist Lighting Design and Install</h1></div>
+						<div className="servSlide"><h1>Plant Rooms & Boiler Houses</h1></div>
+						<div className="servSlide"><h1>Plant Rooms & Boiler Houses</h1></div>
+					</Slider>
+				</div>
 			</div>
 		</section>
 	);
